eb2edffb784a38becca8e870ba975a0eedfc806b
natalia-scr <natalia.scurtu@mail.ru>
2020-02-16 16:18:34 +0000

    Add app main markup and styles

--- src/App.js
+++ src/App.js
@@ -4,9 +4,37 @@ import './App.scss';
 function App() {
   return (
     <div className="main">
-      
+      <div className="hero-background"></div>
+        <div className="searchbox-wrapper">
+          <SearchBox />
+        </div>
     </div>
   );
+}
+
+
+const SearchBox = () => {
+
+  return (
+    <div className="searchbox">
+      <h2 className="searchbox__title">Let’s find your ideal car</h2>
+      <form className="searchbox__form">
+        <label htmlFor="searchbox-input" className="searchbox__label">Pick-up Location</label>
+        <input type="text"
+              name="searchbox-input"
+              id="searchbox-input"
+              className="searchbox__input"
+              value=""
+              onChange={()=>{}}
+              placeholder="city, airport, station, region, district..." 
+              aria-required="true"/>
+        <div className="search-results">
+          
+        </div>
+      </form>
+      
+    </div>
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('given I am a visitor on rentalcars.com homepage', () => {
+
+  it("I should see the search box title and a text box labelled 'Pick-up Location'", () => {
+    const { getByText, getByLabelText } = render(<App />);
+    const searchTitle = getByText(/Let’s find your ideal car/i);
+    expect(searchTitle).toBeInTheDocument();
+
+    const inputBox = getByLabelText(/Pick-up Location/g)
+    expect(inputBox).toBeInTheDocument()
+  })
+  
+  it("On the search box I should see the placeholder text: 'city, airport, station, region and district..", () => {
+    const { getByPlaceholderText } = render(<App />);
+    const placeholderText = getByPlaceholderText(/city, airport, station, region, district.../g)
+
+    expect(placeholderText).toBeInTheDocument();
+  })
+  
 });
+
