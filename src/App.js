b409d170cb16bdfd3dc50ca85013446d3c27a2e3
natalia-scr <natalia.scurtu@mail.ru>
2020-02-16 18:27:31 +0000

    Add react hook to fetch data when user types in the search box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
+const baseUrl = 'https://api.codetabs.com/v1/proxy?quest=';
+const searchResultsNumber = 6;
 
 function App() {
   return (
@@ -15,6 +17,30 @@ function App() {
 
 const SearchBox = () => {
 
+const [query, setQuery] = useState("");
+const [results, setResults] = useState([]);
+
+useEffect(() => {
+  if(query.length < 2) {
+      setResults([])
+
+  } else {
+    async function fetchResults () {
+      const encodedURL = encodeURIComponent(`https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${searchResultsNumber}&solrTerm=${query}`)
+        const response = await fetch(`${baseUrl}${encodedURL}`);
+        const json =  await response.json();
+
+        if(json.results.numFound === 0) {
+          setResults(['none'])
+        } else {
+          setResults(json.results.docs);
+        }
+    }
+    fetchResults()
+  }
+  
+}, [query])
+
   return (
     <div className="searchbox">
       <h2 className="searchbox__title">Let’s find your ideal car</h2>
@@ -24,15 +50,18 @@ const SearchBox = () => {
               name="searchbox-input"
               id="searchbox-input"
               className="searchbox__input"
-              value=""
-              onChange={()=>{}}
+              value={query}
+              onChange={event => setQuery(event.target.value)} 
               placeholder="city, airport, station, region, district..." 
-              aria-required="true"/>
-        <div className="search-results">
-          
+              aria-required="true"
+          />
+        <div className="search-results" data-testid="search-results">
+          { results && results.map((item, i)=> {
+              return <div key={i}>{item.name}</div>
+            })}
+          { results[0] === 'none' && <div>No results found</div> }
         </div>
       </form>
-      
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
 import App from './App';
 
-describe('given I am a visitor on rentalcars.com homepage', () => {
+const nock = require('nock')
+
+describe('given I am a visitor on the search app homepage', () => {
 
   it("I should see the search box title and a text box labelled 'Pick-up Location'", () => {
     const { getByText, getByLabelText } = render(<App />);
@@ -20,5 +22,109 @@ describe('given I am a visitor on rentalcars.com homepage', () => {
     expect(placeholderText).toBeInTheDocument();
   })
   
-});
+  it('When I enter a single character into the pick up location, the placeholder text disappears and no search results are displayed', () => {
+    const { getByLabelText, queryByTestId } = render(<App />);
+    const input = getByLabelText(/Pick-up Location/g);
 
+    fireEvent.change(input, {target : {value : 's'}});
+    expect(input.value).toBe('s')
+    expect(queryByTestId("search-results")).not.toHaveTextContent()
+  })
+
+  it('When 2 or more characters into the pick up location, the search results are displayed', async () => {
+    const searchTerm = 'manchester'
+    const encodedURL = encodeURIComponent(`https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=${searchTerm}`)
+
+    nock('https://api.codetabs.com/v1')
+    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+    .get(`/proxy?quest=${encodedURL}`)
+    .reply(200, {
+      "results": {
+        "docs": [
+          {
+            "bookingId": "city-2810615",
+            "name": "Searcy",
+            "region": "Alabama",
+          },
+          {
+            "bookingId": "city-2810615",
+            "name": "Manchester",
+            "region": "Greater Manchester", 
+          }
+        ]}
+    })
+    const { getByLabelText, queryByTestId, getByText } = render(<App />);
+    const input = getByLabelText(/Pick-up Location/g);
+
+    fireEvent.change(input, {target : {value : searchTerm}});
+    expect(input.value).toBe(searchTerm)
+
+    await waitForElement(() => getByText('Manchester'))
+
+    expect(queryByTestId("search-results")).toHaveTextContent(/Manchester/)
+    expect(queryByTestId("search-results")).toHaveTextContent(/Searcy/)
+  })
+
+  it("When I enter a search term in the pick up location that is not recognised, Then I should see the message 'No results found'", async () => {
+    const searchTerm = 'wtrsetygrtjg'
+    const encodedURL = encodeURIComponent(`https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=${searchTerm}`)
+
+    nock('https://api.codetabs.com/v1')
+    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+    .get(`/proxy?quest=${encodedURL}`)
+    .reply(200, {
+      "results": {
+        numFound: 0
+      }
+    })
+    const { getByLabelText, queryByTestId, getByText } = render(<App />);
+    const input = getByLabelText(/Pick-up Location/g);
+
+    fireEvent.change(input, {target : {value : searchTerm}});
+    expect(input.value).toBe(searchTerm)
+
+    await waitForElement(() => getByText('No results found'))
+
+    expect(queryByTestId("search-results")).toHaveTextContent(/No results found/)
+    
+  })
+
+  it('Given a list of results is displayed, When I remove the search term leaving only 1 character, Then the search results list no longer displayed', async () => {
+    const searchTerm = 'manchester'
+    const encodedURL = encodeURIComponent(`https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=${searchTerm}`)
+
+    nock('https://api.codetabs.com/v1')
+    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+    .get(`/proxy?quest=${encodedURL}`)
+    .reply(200, {
+      "results": {
+        "docs": [
+          {
+            "bookingId": "city-2810615",
+            "name": "Searcy",
+            "region": "Alabama",
+          },
+          {
+            "bookingId": "city-2810615",
+            "name": "Manchester",
+            "region": "Greater Manchester", 
+          }
+        ]}
+    })
+    const { getByLabelText, queryByTestId, getByText } = render(<App />);
+    const input = getByLabelText(/Pick-up Location/g);
+
+    fireEvent.change(input, {target : {value : searchTerm}});
+    expect(input.value).toBe(searchTerm)
+
+    await waitForElement(() => getByText('Manchester'))
+
+    expect(queryByTestId("search-results")).toHaveTextContent(/Manchester/)
+    expect(queryByTestId("search-results")).toHaveTextContent(/Searcy/)
+
+    fireEvent.change(input, {target : {value : 's'}});
+
+    expect(input.value).toBe('s')
+    expect(queryByTestId("search-results")).not.toHaveTextContent()
+  })
+});
